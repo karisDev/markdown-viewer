0f019680b6ea449d76e30d7f9578eb63687b0256
Kirill Kireev <karjaakirill@gmail.com>
2024-08-27 19:31:36 +0300

    reduce js

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,4 +2,7 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	plugins: [require('@tailwindcss/typography'),],
+	experimental: {
+		optimizeUniversalDefaults: true
+	}
 }
